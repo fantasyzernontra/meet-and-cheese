b7a378e8bb977a955688088537a1c0bdfd55610c
fantasyzernontra <fantasyzernontra@hotmail.com>
2021-05-13 00:14:26 +0700

    Initial Project

---
+++ meet-and-cheese-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbfc61cd47cf226b539011711b6bbae8'),
+    },
+  },
+});
